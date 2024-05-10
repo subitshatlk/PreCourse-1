1d12da51d9907e1c956afcf939dd658a18a2c2fc
Subitsha <subitsha@Subitshas-MacBook-Air.local>
2024-05-10 22:22:02 +0530

    Precourse 1 - Exercise 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,12 @@
+//push(int x) : O(1) 
+//pop() : O(1)
+//peek() : O(1)
+//isEmpty() : O(1)
+//Final Time complexity - O(1)
+//Space Complexity : O(n) where n will be the maximum size of the stack
+//problem faced while coding this : Managing stack overflow manually
+//Leetcode : Yes
+
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
@@ -8,30 +17,67 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        //we check if the top is less than 0 as that indicates that the stack is empty.
+        return (top < 0);
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        //initalize top to be -1 whe we create a new stack. 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        //check if top is pointing to the maximum index that is allowed. If it does, then there is a stack overflow. 
+        //Return false to show that we cannot push an element more. 
+        if (top >= (MAX - 1)) { 
+            System.out.println("There is Stack Overflow"); 
+            return false; 
+        } 
+        //If stack is not full, we increment the top pointer and then push the element to the stack. 
+        else { 
+            a[++top] = x; 
+            System.out.println(x + " is pushed into stack"); 
+            return true; 
+        } 
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        //If stack is empty, then we cannot pop an element hence return false. 
+        if (isEmpty()) { 
+            System.out.println("Stack Underflow"); 
+            return 0; 
+        } 
+        //If stack is not empty, remove the element by decrementing the top pointer
+        else { 
+            int x = a[top--]; 
+        //return the top element
+            return x; 
+        } 
     } 
   
     int peek() 
     { 
         //Write your code here
+        //if stack is empty, then there is not top element to return.
+        if (isEmpty()) { 
+            System.out.println("Stack Underflow"); 
+            return 0; 
+        } 
+        // return the element which is at the top. 
+        else { 
+            int x = a[top]; 
+            return x; 
+        } 
     } 
-} 
+    } 
   
 // Driver code 
 class Main { 
